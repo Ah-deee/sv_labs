89fc3f2a6a75c48a2325875f44177e604ace4428
Ahdee <anadithya1@gmail.com>
2026-09-24 17:26:02 +0530

    added modports and added if to dut

--- UART/UART.sv
+++ UART/UART.sv
@@ -1,24 +1,14 @@
 module UART#(
     parameter clk_freq  = 50_000_000,
     parameter baud_rate = 9600
-)(
-    input logic rst,
-    input logic clk, 
-    input logic wr_en, 
-    input logic [7:0] data_in, 
-    
-    input logic rdy_clr,
-
-    output rdy,busy,
-    output logic [7:0] data_out
-);
+)(UART_if.dut bus);
 
 logic rx_tick;
 logic tx_tick;
 logic tx_rx; //output of tx module and input of the rx module
 
-baud_rate_generator bg(.clk(clk),.rst(rst),.tx_tick(tx_tick),.rx_tick(rx_tick));
-transmitter_block tb(.clk(clk),.rst(rst),.wr_en(wr_en),.tx_tick(tx_tick),.data_in(data_in),.tx(tx_rx),.busy(busy));
-receiver_block rb(.clk(clk),.rst(rst),.rx_tick(rx_tick),.rx(tx_rx),.data_out(data_out),.rdy(rdy),.rdy_clr(rdy_clr));
+baud_rate_generator bg(.clk(bus.clk),.rst(bus.rst),.tx_tick(tx_tick),.rx_tick(rx_tick));
+transmitter_block tb(.clk(bus.clk),.rst(bus.rst),.wr_en(bus.wr_en),.tx_tick(tx_tick),.data_in(bus.data_in),.tx(tx_rx),.busy(bus.busy));
+receiver_block rb(.clk(bus.clk),.rst(bus.rst),.rx_tick(rx_tick),.rx(tx_rx),.data_out(bus.data_out),.rdy(bus.rdy),.rdy_clr(bus.rdy_clr));
 
 endmodule
--- UART/UART_if.sv
+++ UART/UART_if.sv
@@ -2,6 +2,8 @@ interface UART_if(input bit clk);
 logic rst,wr_en,rdy_clr,rdy,busy;
 logic [7:0] data_in,data_out;
 
-modport dut(input rst,wr_en,rdy_clr, input [7:0] data_in, output [7:0] data_out,output rdy,busy);
-modport tb(input [7:0] data_out, input rdy,busy, output rst,wr_en,rdy_clr,output [7:0] data_in);
+modport dut(input rst,wr_en,rdy_clr,clk, data_in, 
+            output data_out,rdy,busy);
+modport tb(input data_out,rdy,busy, 
+            output rst,wr_en,rdy_clr,data_in,clk);
 endinterface
